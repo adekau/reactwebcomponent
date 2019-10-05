fd84bc858af5d2e6658e68120c779bf6eee159ec
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2019-10-05 01:58:25 -0400

    Pretty much working state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ReactCustomElement';
+export class AppComponent implements OnInit {
+  title = 'test title attr';
+
+  ngOnInit() {
+    setTimeout(() => { this.title = "It changed!!"; }, 5000);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { EuiButtonWC } from './app/eui-wc/eui-button.webcomponent';
+import { EuiSuperDatePickerWC } from './app/eui-wc/eui-superdatepicker.webcomponent';
+
+window.customElements.define('eui-button-wc', EuiButtonWC);
+window.customElements.define('eui-super-date-picker-wc', EuiSuperDatePickerWC);
+
 if (environment.production) {
   enableProdMode();
 }
